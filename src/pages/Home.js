d03da8ad0a93ed4bd1ec88bf20b007456c3c061b
Alexey Kudryashov <akd@anadeainc.com>
2023-05-12 16:56:59 +0300

    Main banner first part

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return(
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img
+                  alt="main banner"
+                  className="img-fluid rounded-3"
+                  src="/images/main-banner-1.jpg"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>BEST TOYS EVER</h4>
+                  <h5>HASBRO CAR</h5>
+                  <p>From $999.00 or 41.62/mo</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
